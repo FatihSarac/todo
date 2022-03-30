992d2e1af6662da88bf3214e28a9f640cc34248a
Fatih Saraç <fatih.sarac@neyasis.com>
2022-03-30 23:22:26 +0300

    bugfix

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import { Grid } from "@mui/material";
 import "./App.css";
 import Home from "./pages/Home/Home";
@@ -5,7 +7,11 @@ import Home from "./pages/Home/Home";
 function App() {
   return (
     <Grid>
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" exact={true} element={<Home />} />
+        </Routes>
+      </Router>
     </Grid>
   );
 }
